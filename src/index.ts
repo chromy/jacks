9219aaec3da339107b4c1fe6c776cda230a6896e
Hector Dearman <hector.dearman@gmail.com>
2024-07-13 20:04:58 +0100

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { extractText, getDocumentProxy } from "unpdf";
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
 export interface Env {
   GEMINI_API_KEY: string;
-
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   JACKS: KVNamespace;
-
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
 }
 
 const KV_CACHED_RESPONSE_KEY = "CACHED_RESPONSE_KEY";
@@ -32,7 +11,7 @@ const MENU_URL = "https://docs.google.com/document/d/1kDBSxPb8X4L2TKXWUmm2A-VGuP
 
 const TIMEOUT_MS = 60 * 60 * 1000;
 
-const generationConfig = {
+const GENERATION_CONFIG = {
   temperature: 1,
   topP: 0.95,
   topK: 64,
@@ -68,9 +47,7 @@ async function convertMenuToJson(geminiApiKey: string, menu: string) {
   });
 
   const chatSession = model.startChat({
-    generationConfig,
-    // safetySettings: Adjust safety settings
-    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    GENERATION_CONFIG,
     history: []
   });
 
